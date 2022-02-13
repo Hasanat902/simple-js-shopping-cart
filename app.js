4adbcfcc33c92b25da212ef37732411a7c3b9ad8
Hasanat902 <abulhasanat902@gmail.com>
2022-02-13 21:28:50 +0600

    product increase decrease added

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('I am in a separate file')
+function updatePhoneNumber(product, price, isPositive){
+    const productInput = document.getElementById(product + "-number");
+    const productNumber = productInput.value;
+    if(isPositive){
+        const newProductNumber = parseInt(productNumber) + 1;
+        productInput.value = newProductNumber;
+    }
+    else if(productNumber > 0){
+        const newCaseNumber = parseInt(productNumber) - 1;
+        productInput.value = newCaseNumber;
+    }
+    const productTotal = document.getElementById(product + "-price");
+    productTotal.innerText = productInput.value * price;
+    
+}
+
+document.getElementById("case-plus").addEventListener("click", function(){
+    
+    updatePhoneNumber("case", 59, true);
+});
+
+document.getElementById("case-minus").addEventListener("click", function(){
+    
+   updatePhoneNumber("case", 59, false);
+});
+
+document.getElementById("phone-plus").addEventListener("click", function(){
+    updatePhoneNumber("phone", 1219, true);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function(){
+    updatePhoneNumber("phone", 1219, false);
+});
